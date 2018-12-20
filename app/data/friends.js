0bdadf923168b6f9fde6d8a093b5b2ab189233e6
Kevin <kobbootcamp@gmail.com>
2018-12-20 13:08:45 -0600

    MVP

---
+++ app/data/friends.js
@@ -0,0 +1,93 @@
+var friends = [
+    {
+        "name":"George",
+        "photo":"http://www2.pictures.zimbio.com/gi/George+Clooney+John+Wells+Honored+Hollywood+HKkg1klcBDVl.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ],
+          "total": 32
+      },    
+      {
+        "name":"Sophia",
+        "photo":"https://static.tvgcdn.net/mediabin/showcards/celebs/s-u/thumbs/sofia-vergara_sc_768x1024.png",
+        "scores":[
+            3,
+            2,
+            5,
+            1,
+            2,
+            1,
+            4,
+            5,
+            4,
+            1
+          ],
+          "total": 27
+      },
+      {
+        "name":"Tim",
+        "photo":"https://wdef.com/wp-content/uploads/2018/08/tim-tebow-1.jpg",
+        "scores":[
+            3,
+            1,
+            4,
+            3,
+            5,
+            1,
+            2,
+            1,
+            1,
+            1
+          ],
+          "total": 22
+      },
+      {
+        "name":"Mona",
+        "photo":"https://d2jv9003bew7ag.cloudfront.net/uploads/Leonardo-da-Vinci-Mona-Lisa-detail-865x577.jpg",
+        "scores":[
+            2,
+            1,
+            2,
+            3,
+            1,
+            1,
+            2,
+            1,
+            2,
+            1
+          ],
+          "total": 16
+      },
+      {
+        "name":"Danica",
+        "photo":"https://usatftw.files.wordpress.com/2018/05/usp-indycar_-102nd-running-of-the-indianapolis-500_99998681.jpg?w=1000&h=600&crop=1",
+        "scores":[
+            2,
+            1,
+            1,
+            1,
+            1,
+            1,
+            2,
+            1,
+            1,
+            1
+          ],
+          "total": 12
+      }
+      
+];
+    
+
+
+https://usatftw.files.wordpress.com/2018/05/usp-indycar_-102nd-running-of-the-indianapolis-500_99998681.jpg?w=1000&h=600&crop=1
+module.exports = friends;
